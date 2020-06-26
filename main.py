569a8c47757195e7822a5d75b62df76e79c59384
Danil <sthfaceless@mail.ru>
2020-06-26 14:37:48 +0400

    One dimensional linear regression updated

--- main.py
+++ main.py
@@ -3,4 +3,8 @@ import numpy as np
 from scipy.special import gamma
 from scipy.special import gammainc
 from scipy.special import gammaincinv
+from scipy.special import erfinv
 
+
+val = 0.48
+print(erfinv(val * 2) * (2 ** 0.5))
